01780ad6cbe2b9985950ec43c57221c396d9cde5
ricardoAguirreSanchez <ricardo.aguirre.99@gmail.com>
2017-04-06 15:43:41 -0300

    Lo dejo comentado ya que por ahora no se usaran

--- src/main/java/com/ar/utn/services/mock/ServicioMock.java
+++ src/main/java/com/ar/utn/services/mock/ServicioMock.java
@@ -8,7 +8,7 @@ import com.ar.utn.dto.CuentaDTO;
 
 
 public interface ServicioMock {
-	
-	public ArrayList<CuentaDTO> consultaCuentasPor(String nombreEmpresa, String periodo, int anio);
-	public ArrayList<CuentaDTO> consultaCuentasTotales();
+//	Lo dejo comentado ya que actualmete ya existe una interface y una clase servicio con estos metodos	
+//	public ArrayList<CuentaDTO> consultaCuentasPor(String nombreEmpresa, String periodo, int anio);
+//	public ArrayList<CuentaDTO> consultaCuentasTotales();
 }
