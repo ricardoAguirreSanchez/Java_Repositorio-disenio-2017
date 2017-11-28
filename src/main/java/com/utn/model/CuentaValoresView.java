3367a0fa3ddaab4f85b89d7106531f3baa4dc0c7
Sebastian <Sebastian@192.168.0.117>
2017-11-28 19:23:12 -0300

    Me olvide esto...(Objeto nuevo)

---
+++ src/main/java/com/utn/model/CuentaValoresView.java
@@ -0,0 +1,29 @@
+package com.utn.model;
+
+public class CuentaValoresView {
+	private long indicadorId;
+	private long cuentaId;
+	private long valorCuentaId;
+	
+	
+	public long getIndicadorId() {
+		return indicadorId;
+	}
+	public void setIndicadorId(long indicadorId) {
+		this.indicadorId = indicadorId;
+	}
+	public long getCuentaId() {
+		return cuentaId;
+	}
+	public void setCuentaId(long cuentaId) {
+		this.cuentaId = cuentaId;
+	}
+	public long getValorCuentaId() {
+		return valorCuentaId;
+	}
+	public void setValorCuentaId(long valorCuentaId) {
+		this.valorCuentaId = valorCuentaId;
+	}
+	
+	
+}
